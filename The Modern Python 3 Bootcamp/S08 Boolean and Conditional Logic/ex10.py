8df2d0e099f37a643df2d144b238e21570055231
Michael Allen <michael@allenhub.net>
2019-04-05 14:52:34 -0400

    catching up code exercices

---
+++ The Modern Python 3 Bootcamp/S08 Boolean and Conditional Logic/ex10.py
@@ -0,0 +1,17 @@
+# NO TOUCHING ============================================
+from random import choice
+food = choice(['apple','grape', 'bacon', 'steak', 'worm', 'dirt'])
+# NO TOUCHING =============================================
+
+
+# YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+if("apple" == food or "grape" == food):
+    print("fruit")
+elif("bacon" == food or "steak" == food):
+    print("meat")
+elif("dirt" == food or "worm" == food):
+    print()
+
+# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
---
+++ The Modern Python 3 Bootcamp/S08 Boolean and Conditional Logic/ex8.py
@@ -0,0 +1,11 @@
+# NO TOUCHING PLEASE---------------
+from random import randint
+choice = randint(1,10)
+# NO TOUCHING PLEASE---------------
+
+# YOUR CODE GOES HERE:
+if(7 == choice):
+    print("lucky")
+else:
+    print("unlucky")
+    
